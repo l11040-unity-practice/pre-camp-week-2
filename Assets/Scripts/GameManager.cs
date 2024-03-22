89b7eb36009b306274fd54142772157d27911535
l11040 <113540683+l11040@users.noreply.github.com>
2024-03-22 17:28:20 +0900

    Update : 시간 구

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject Square;
+    public Text TimeText;
+
+    float time = 0.0f;
+
     void Start()
     {
         InvokeRepeating("MakeSquare", 0f, 1.0f);
@@ -12,7 +17,8 @@ public class GameManager : MonoBehaviour
 
     void Update()
     {
-
+        time += Time.deltaTime;
+        TimeText.text = time.ToString("N2");
     }
 
     void MakeSquare()
